4dab8bc5981b25290258109ba1e948f1317ebdd8
Nik Butenko <nik@butenko.me>
2015-02-22 15:56:33 +1100

    Add Plato report

---
+++ doc/plato/files/lib_ui_state_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sun, 22 Feb 2015 04:55:50 GMT","sloc":98,"lloc":53,"functions":8,"deliveredBugs":0.5986005127229501,"maintainability":69.95377123309775,"lintErrors":[],"difficulty":24.76923076923077}]
